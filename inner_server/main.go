e3c92529d4def2138ec6641a596a0590b97cef32
Administrator <691207572@qq.com>
2020-05-28 14:22:20 +0800

    fix bug:断线后内网服务打开多个到外网服务器的连接

--- inner_server/main.go
+++ inner_server/main.go
@@ -13,10 +13,12 @@ import (
 type ServerInner struct {
 	RegisterAddress string
 	AuthKey         string
-	connId          uint64
-	connNum         int64
-	ConnList        sync.Map //到外网服务的连接
-	ProxyAddress    string
+	connId          uint64    // 当前连接Id
+	connFree        sync.Map  //空闲连接
+	connMaxFree     int       //最大空闲连接数
+	connChangeSign  chan bool //连接增加或减少
+	ConnList        sync.Map  //到外网服务的连接
+	ProxyAddress    string    //转发地址
 }
 
 // 连接id生成
@@ -45,32 +47,56 @@ func (si *ServerInner) batchPing() {
 	}
 }
 
+// 检查连接数量
+func (si *ServerInner) connectionNumCheck() {
+	for {
+		select {
+		case <-si.connChangeSign:
+			freeNum := 0
+			si.connFree.Range(func(key, value interface{}) bool {
+				freeNum++
+				return true
+			})
+			newNum := si.connMaxFree - freeNum
+			if newNum > 0 {
+				go si.batchConnectToOuter(newNum)
+			}
+		}
+	}
+}
+
 // 批量创建新连接到外网服务器
 func (si *ServerInner) batchConnectToOuter(num int) {
+	cidArr := make([]uint64, num)
 	for i := 1; i <= num; i++ {
+		cid := si.generateConnId()
+		cidArr = append(cidArr, cid)
 		c := &connection.InnerToOuterConnection{
-			Id: si.generateConnId(),
+			Id: cid,
 			StatusMonitor: func(id uint64, status int) {
 				switch status {
+				case connection.StatusProxy:
+					si.connFree.Delete(id)
 				case connection.StatusClose:
+					si.connFree.Delete(id)
 					si.ConnList.Delete(id)
-					if si.connNum > 0 {
-						si.connNum--
-					}
-					if si.connNum < 10 {
-						si.batchConnectToOuter(10)
-					}
 				}
+				si.connChangeSign <- true
 			},
 			OutServerAddress:       si.RegisterAddress,
 			OutServerAuthKey:       si.AuthKey,
 			OutServerConnWriteLock: sync.Mutex{},
 			ProxyAddress:           si.ProxyAddress,
 		}
-		c.Register()
-		go c.Read()
-		si.connNum++
-		si.ConnList.Store(c.Id, c)
+		si.ConnList.Store(cid, c)
+		si.connFree.Store(cid, true)
+	}
+	for _, cid := range cidArr {
+		c, ok := si.ConnList.Load(cid)
+		if ok {
+			c.(*connection.InnerToOuterConnection).Register()
+			go c.(*connection.InnerToOuterConnection).Read()
+		}
 	}
 }
 
@@ -107,8 +133,11 @@ func main() {
 		RegisterAddress: registerAddress,
 		AuthKey:         c.AuthKey,
 		ProxyAddress:    proxyAddress,
+		connMaxFree:     10,
+		connChangeSign:  make(chan bool, 90),
 	}
-	si.batchConnectToOuter(10)
 	log.Println("start success")
+	si.connChangeSign <- true
+	go si.connectionNumCheck()
 	si.batchPing()
 }
